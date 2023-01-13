b2e4796d8319f7fab6fd3fcccd95a3cacd0e5e2a
Matheus Diógenes <mathdiogenes@gmail.com>
2023-01-13 18:46:29 -0300

    Req 06 em andamento

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -3,20 +3,17 @@ const validateSalesInputFields = require('../validations/validateSalesInputField
 
 const createSales = async (salesToCreate) => {
   const error = validateSalesInputFields(salesToCreate);
-  if (error.type) return error;
-  // if (salesToCreate.some(({ productId }) => !productId)) {
-  //   return { type: 'BAD_REQUEST', message: '"productId" is required' };
-  // }
+  if (error) return error;
 
-  // const allProductsAreAvailable = Promise.all(
-  //   salesToCreate.map(({ productId }) => productsModel.getById(productId)),
-  // );
-  // if (allProductsAreAvailable.length !== salesToCreate.length) {
-  //   return {
-  //       type: 'PRODUCT_NOT_FOUND',
-  //       message: 'Product not found',
-  //   }; 
-  // }
+  const allProductsAreAvailable = Promise.all(
+    salesToCreate.map(({ productId }) => productsModel.getById(productId)),
+  );
+  if (allProductsAreAvailable.length !== salesToCreate.length) {
+    return {
+        type: 'PRODUCT_NOT_FOUND',
+        message: 'Product not found',
+    }; 
+  }
 
   const saleId = await salesModel.createSaleId();
   const saleInfo = await salesModel.createSaleInfo(saleId, salesToCreate);
--- src/validations/validateSalesInputFields.js
+++ src/validations/validateSalesInputFields.js
@@ -1,4 +1,10 @@
 module.exports = (salesToCreate) => {
+  if (salesToCreate.some(({ quantity }) => quantity < 1)) {
+    return {
+      type: 'INVALID_INPUT',
+      message: '"quantity" must be greater than or equal to 1',
+    };
+  } 
   if (salesToCreate.some(({ productId }) => !productId)) {
     return { type: 'BAD_REQUEST', message: '"productId" is required' };
   }
@@ -6,11 +12,4 @@ module.exports = (salesToCreate) => {
   if (salesToCreate.some(({ quantity }) => !quantity)) {
     return { type: 'BAD_REQUEST', message: '"quantity" is required' };
   }
-
-  if (salesToCreate.some(({ quantity }) => quantity < 1)) {
-    return {
-      type: 'INVALID_INPUT',
-      message: '"quantity" must be greater than or equal to 1',
-    };
-  } 
 };
