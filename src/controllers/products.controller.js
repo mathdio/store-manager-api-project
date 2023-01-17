0960116e7c752cec35924c60ec72cfa41294db56
Matheus Diógenes <mathdiogenes@gmail.com>
2023-01-17 15:21:17 -0300

    feat: Products controller unit tests (getById function)

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -4,7 +4,7 @@ const { productsService } = require('../services');
 const getProducts = async (_req, res) => {
   const { message } = await productsService.getProducts();
 
-  res.status(200).json(message);
+  return res.status(200).json(message);
 };
 
 const getById = async (req, res) => {
--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -4,7 +4,7 @@ const sinonChai = require('sinon-chai');
 
 const { productsController } = require('../../../src/controllers');
 const { productsService } = require('../../../src/services');
-const { productsList } = require('./mocks/products.controller.mock');
+const { productsList, singleProduct } = require('./mocks/products.controller.mock');
 
 const { expect } = chai;
 chai.use(sinonChai);
@@ -12,9 +12,10 @@ chai.use(sinonChai);
 describe('Products Controller\'s unit tests', function () {
   afterEach(sinon.restore);
 
-  describe('List of all products', function () {
-    it('If it returns an successfully response', async function () {
-      sinon.stub(productsService, 'getProducts').resolves({ type: null, message: productsList });
+  describe('List all products', function () {
+    it('If it returns a successful response', async function () {
+      const mockResolve = { type: null, message: productsList };
+      sinon.stub(productsService, 'getProducts').resolves(mockResolve);
 
       const req = {};
       const res = {};
@@ -24,6 +25,36 @@ describe('Products Controller\'s unit tests', function () {
       await productsController.getProducts(req, res);
       expect(res.status).to.have.been.calledOnceWith(200);
       expect(res.json).to.have.been.calledOnceWith(productsList);
+    });
+  });
+
+  describe('List a product by id', function () {
+    it('If it returns a successful response', async function () {
+      const mockResolve = { type: null, message: singleProduct };
+      sinon.stub(productsService, 'getById').resolves(mockResolve);
+
+      const req = { params: { id: 1 } };
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      await productsController.getById(req, res);
+      expect(res.status).to.have.been.calledOnceWith(200);
+      expect(res.json).to.have.been.calledOnceWith(singleProduct);
+    });
+
+    it("If it returns a PRODUCT_NOT_FOUND error", async function () {
+      const mockResolve = { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+      sinon.stub(productsService, 'getById').resolves(mockResolve);
+
+      const req = { params: { id: 999 } };
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      await productsController.getById(req, res);
+      expect(res.status).to.have.been.calledOnceWith(404);
+      expect(res.json).to.have.been.calledOnceWith({ message: 'Product not found' });
     });
   });
 });
